2ce4255dbece7d60f831eaa31d71b27eb61ceb92
yenniferwilanata <yenniferwilanata@gmail.com>
2023-09-20 20:46:33 +0700

    bkin login register menu dan css untuk login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Log In</title>
+        <link href="login.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class = "loginpage">
+            <div class = "image">
+                <img src = "https://media.istockphoto.com/id/545286388/id/foto/latar-belakang-kosong-makanan-cina.jpg?s=612x612&w=0&k=20&c=cP30wn8oY16u-7wAe_vlnDhDNaGpRuFYtDDKDHKLMQk=">
+            </div>
+            <div class = "content">
+                <form>
+                    <h2 class = "title">Login</h2>
+                    <article class = "email">
+                        Email Address
+                    </article>
+                    <input class = "fillIn" type = "text" placeholder = "Has to end with '@gmail.com'" name = "email" required>
+                    <article class = "password">
+                        Password
+                    </article>
+                    <input class = "fillIn" type = "text" placeholder = "Minimum 5 Characters, Maximum 255 Characters" name = "password">
+                    <input type = "checkbox"><article class = "rememberMe">Remember Me</article>
+                    <button type = "submit">
+                        Login
+                    </button>
+                    <p class = "ask">Don't have an account?</p><p class = "signup"> Sign up</p>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ resources/views/register.blade.php
@@ -0,0 +1,31 @@
+<h2>
+    Register
+    <form>
+        <label for = "email">
+            <b>Email Address</b>
+        </label>
+        <input type = "email" placeholder = "Has to end with '@gmail.com'" name = "email" required>
+        
+        <label for = "username">
+            <b>Username</b>
+        </label>
+        <input type = "username" placeholder = "Minimum 5 Characters, Maximum 50 Characters">
+        
+        <label for = "password">
+            <b>Password</b>
+        </label>
+        <input type = "password" placeholder = "Minimum 5 Characters, Maximum 255 Characters">
+        
+        <label for = "conPassword">
+            <b>Confirm Password</b>
+        </label>
+        <input type = "conPassword" placeholder = "Has to be the same with Password Field">
+        
+        <button type = "submit">
+            Register
+        </button>
+    </form>
+    <h5>
+        Already have an account? Login
+    </h5>
+</h2>
