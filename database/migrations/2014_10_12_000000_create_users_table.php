ffebe3a8578bc49f096844b5910d0b0cdea46241
raphael-lesmana <65525536+raphael-lesmana@users.noreply.github.com>
2023-09-21 20:12:48 +0700

    add controllers, remove tables

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('phone_number', 12)->nullable();
+            $table->string('address', 64)->nullable();
+            $table->string('picture', 32)->nullable();
+            $table->boolean('is_admin');
             $table->timestamps();
         });
     }
