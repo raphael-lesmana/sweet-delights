0862aff010ccc1ae4fbe8da705a1d0bf606250e8
yenniferwilanata <yenniferwilanata@gmail.com>
2023-09-22 13:21:47 +0700

    perubahan

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -10,22 +10,24 @@
 <body>
     <div class="registerpage">
         <img src="https://media.istockphoto.com/id/545286388/id/foto/latar-belakang-kosong-makanan-cina.jpg?s=612x612&w=0&k=20&c=cP30wn8oY16u-7wAe_vlnDhDNaGpRuFYtDDKDHKLMQk=">
-        <h1>Register</h1>
-            <form action="/register" method="post">
-                <label for = "email">Email Address</label>
-                <input type = "email" placeholder = "Has to end with '@gmail.com'" name = "email" required>
-                
-                <label for = "username">Username</label>
-                <input type = "username" placeholder = "Minimum 5 Characters, Maximum 50 Characters">
-                
-                <label for = "password">Password</label>
-                <input type = "password" placeholder = "Minimum 5 Characters, Maximum 255 Characters">
-                
-                <label for = "conPassword">Confirm Password</label>
-                <input type = "conPassword" placeholder = "Has to be the same with Password Field">
-                
-                <button type = "submit">Register</button>
-            </form>
+        <h1 class="title">Register</h1>
+
+        <form action="/register" method="post">
+            <label for = "email">Email Address</label>
+            <input type = "email" placeholder = "Has to end with '@gmail.com'" name = "email" required/>
+            
+            <label for = "username">Username</label>
+            <input type = "username" placeholder = "Minimum 5 Characters, Maximum 50 Characters"/>
+            
+            <label for = "password">Password</label>
+            <input type = "password" placeholder = "Minimum 5 Characters, Maximum 255 Characters"/>
+            
+            <label for = "conPassword">Confirm Password</label>
+            <input type = "conPassword" placeholder = "Has to be the same with Password Field">
+            
+            <button type = "submit">Register</button>
+        </form>
+
         <p>Already have an account? Login</p>
     </div>
 </body>
