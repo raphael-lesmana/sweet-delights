e74ba6ec77e7035caceeac6136c1d4f64b91a93d
raphael-lesmana <65525536+raphael-lesmana@users.noreply.github.com>
2023-09-28 20:09:12 +0700

    seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,22 +19,17 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'email' => fake()->userName() . '@gmail.com',
+            'password' => 'password', // password
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
+    public function admin()
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
+        return $this->state(function (array $attributes) {
+            return [
+                'is_admin' => true,
+            ];
+        });
     }
 }
--- database/migrations/2023_09_28_114549_items.php
+++ database/migrations/2023_09_28_114549_items.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('items', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->enum('type', ['main', 'beverage', 'dessert']);
