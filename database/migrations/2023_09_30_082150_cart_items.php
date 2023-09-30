5fd2a85d77bff5620f2952ce9300f0e6468936fc
raphael-lesmana <65525536+raphael-lesmana@users.noreply.github.com>
2023-09-30 15:41:14 +0700

    start cart and transactions table

---
+++ database/migrations/2023_09_30_082150_cart_items.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->foreignId('item_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->integer('qty')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};
