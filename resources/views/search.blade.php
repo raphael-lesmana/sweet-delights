e47347ca3a5693867a46e3de3eda3c59f38b8b73
yenniferwilanata <yenniferwilanata@gmail.com>
2023-11-21 20:50:30 +0700

    search

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -13,12 +13,14 @@
         <div class="mb-3">
             <input class="form-control me-2" type="search" placeholder="Search" name="search" style="width: 700px">
         </div>
-        <div class="mb-3 form-check" style="color: white">
+        <div class = "mb-3 radio-inline" style="color: white">
             <input type="checkbox" id="main-course" class = "form-check-input" style="margin-left: 10px" name="main">
             <label for="main-course" class="form-check-label" >Main Course</label>
-            <input type="checkbox" id="beverages" style="margin-left: 10px" name="beverages">
+
+            <input type="checkbox" id="beverages" class = "form-check-input" style="margin-left: 10px" name="beverages">
             <label for="beverages">Beverages</label>
-            <input type="checkbox" id="dessert" style="margin-left: 10px" name="dessert">
+
+            <input type="checkbox" id="dessert" class = "form-check-input" style="margin-left: 10px" name="dessert">
             <label for="dessert">Dessert</label>
         </div>
         <button class="btn btn-dark">Search</button>
