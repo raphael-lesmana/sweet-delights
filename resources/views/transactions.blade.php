d00be17cec1456f329730b0c525544df7f18b416
raphael-lesmana <65525536+raphael-lesmana@users.noreply.github.com>
2023-11-26 18:59:51 +0700

    transactions fix

--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -1,6 +1,10 @@
 <h1>Transactions</h1>
 <ul>
-    @foreach ($transactions as $transaction)
-        <li> TR{{$transaction->transactionHeader->id}} {{date_format($transaction->created_at, "Y-m-d")}} {{$transaction->item->name}} [x{{$transaction->qty}}] ${{$transaction->item->price * $transaction->qty}}</li>
-    @endforeach
+    @if (sizeof($transactions) == 0)
+        <img src="http://memegen.link/custom/No Transactions%3F.jpg?alt=https://i.imgflip.com/6aoqqy.jpg" width="200px">
+    @else
+        @foreach ($transactions as $transaction)
+            <li> TR{{$transaction->transactionHeader->id}} {{date_format($transaction->created_at, "Y-m-d")}} {{$transaction->item->name}} [x{{$transaction->qty}}] ${{$transaction->item->price * $transaction->qty}}</li>
+        @endforeach
+    @endif
 </ul>
