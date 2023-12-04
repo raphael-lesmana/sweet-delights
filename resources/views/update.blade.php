0756523571b4fdd1fd784a4e947dedd77e6f19be
yenniferwilanata <yenniferwilanata@gmail.com>
2023-12-04 22:38:02 +0700

    update

--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -1,33 +1,79 @@
-<h1>Update Food</h1>
-<form action="{{ Request::url() }}" method="POST" enctype="multipart/form-data">
-    @csrf
-    <input type="hidden" name="_method" value="PATCH">
-    <label for="name">Food name</label>
-    <input type="text" name="name" placeholder="Minimum 5 characters" value="{{$item->name}}"><br>
-    <label for="brief_description">Brief description</label>
-    <textarea name="brief_description" placeholder="Maximum 100 characters">{{$item->brief_description}}</textarea><br>
-    <label for="brief_description">Full description</label>
-    <textarea name="full_description" placeholder="Maximum 255 characters">{{$item->full_description}}</textarea><br>
-    <label for="brief_description">Category</label>
-    <select name="type">
-        <option value="Main Course"
-        @if ($item->type == "Main Course")
-            selected
-        @endif
-        >Main dish</option>
-        <option value="Beverage"
-        @if ($item->type == "Beverage")
-            selected
-        @endif>
-        Beverage</option>
-        <option value="Dessert" 
-        @if ($item->type == "Dessert")
-            selected
-        @endif>Dessert</option>
-    </select><br>
-    <label for="brief_description">Price</label>
-    <input type="number" name="price" value="{{$item->price}}"><br>
-    <label for="picture">Image file</label>
-    <input type="file" name="picture"><br>
-    <button>Update</button>
-</form>
+@extends('include.navbar')
+
+@section('title', 'Update Food')
+
+@section('content')
+
+<div class="card text-gold col-6 mx-auto mb-5" style="background-color:black; color: gold; font-size: 30px; text-align: center; font-weight: bold">
+    更新食物 | Update Food
+</div>
+
+<div class="col-6 mx-auto">
+    <form action="{{ Request::url() }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <input type="hidden" name="_method" value="PATCH">
+
+        <div class="mb-3 mx-auto" style="text-align:left">
+            <label for="name" class="form-label" style="color: gold; font-size: 20px">
+                <div class="card text-gold" style="background-color: black">Food Name</div>
+            </label>
+            <input type="text" class="form-control" name="name" placeholder="Mininum 5 Characters" value="{{$item->name}}"></textarea>
+        </div>
+
+        <div class="mb-3 mx-auto" style="text-align:left">
+            <label for="brief_description" class="form-label" style="color: gold; font-size: 20px">
+                <div class="card text-gold" style="background-color: black">Brief Description</div>
+            </label>
+            <textarea type="text" class="form-control" name="brief_description" placeholder="Maximum 100 characters">{{$item->brief_description}}</textarea>
+        </div>
+
+        <div class="mb-3 mx-auto" style="text-align:left">
+            <label for="full_description" class="form-label" style="color: gold; font-size: 20px">
+                <div class="card text-gold" style="background-color: black">Brief Description</div>
+            </label>
+            <textarea type="text" class="form-control" name="full_description" placeholder="Maximum 255 characters">{{$item->full_description}}</textarea>
+        </div>
+
+        <div class="mb-3 mx-auto" style="text-align:left">
+            <label for="category" class="form-label" style="color: gold; font-size: 20px">
+                <div class="card text-gold" style="background-color: black">Food Category</div>
+            </label><br>
+            <select class="mx-auto" name="type" style="font-size: 20px; width: 100%; height: 35px">
+                <option value="Main Course"
+                @if ($item->type == "Main Course")
+                    selected
+                @endif
+                >Main dish</option>
+                <option value="Beverage"
+                @if ($item->type == "Beverage")
+                    selected
+                @endif>
+                Beverage</option>
+                <option value="Dessert" 
+                @if ($item->type == "Dessert")
+                    selected
+                @endif>Dessert</option>
+            </select>
+        </div>
+
+        <div class="mb-3 mx-auto" style="text-align:left">
+            <label for="price" class="form-label" style="color: gold; font-size: 20px">
+                <div class="card text-gold" style="background-color: black">Price</div>
+            </label>
+            <input type="number" class="form-control" name="price" value="{{$item->price}}">
+        </div>
+
+        <div class="mb-3">
+            <label for="picture" class="form-label" style="color: gold; font-size: 20px">
+                <div class="card text-gold" style="background-color: black">Food Image</div>
+            </label>
+            <input class="form-control" type="file" id="picture" name="picture">
+        </div>
+
+        <div style="text-align: right">
+            <button type="submit" class="btn" style="color: white; background-color: black">Update Food</button>
+        </div>
+    </form>
+</div>
+
+@endsection
