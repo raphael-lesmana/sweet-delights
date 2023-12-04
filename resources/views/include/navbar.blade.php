2fff659f93b1fed60a3f836c146e96011ed2a09b
yenniferwilanata <yenniferwilanata@gmail.com>
2023-12-04 14:36:26 +0700

    navbar

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -10,14 +10,14 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid"> 
-            <a class="navbar-brand" href="/" style="color: gold">
+            <a class="navbar-brand" href="/" style="color: gold; font-weight: bold">
                 XiAO DiNG DoNG
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <div class="collapse navbar-collapse" id="navbarNavDropdown" style="font-weight: bold;">
                 <ul class="navbar-nav">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="/">Home</a>
